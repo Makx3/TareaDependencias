cfe08d985652196026a0b07994e00a22f76e1da1
Makx3 <142506889+Makx3@users.noreply.github.com>
2023-10-16 03:48:17 -0300

    Add files via upload

---
+++ src/main/java/Dependencias/Main.java
@@ -0,0 +1,9 @@
+package Dependencias;
+
+import Dependencias.InteraccionUsuario;
+
+public class Main {
+    public static void main(String[] args) {
+        InteraccionUsuario.ejecutarInteraccion();
+    }
+}
