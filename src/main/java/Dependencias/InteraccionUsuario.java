e50b0be0a61c603923ddc53484014c3fd5b9c122
Makx3 <142506889+Makx3@users.noreply.github.com>
2023-10-16 04:11:57 -0300

    Update InteraccionUsuario.java

--- src/main/java/Dependencias/InteraccionUsuario.java
+++ src/main/java/Dependencias/InteraccionUsuario.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class InteraccionUsuario {
     public static void ejecutarInteraccion() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("ingrese su nombre: ");
+        System.out.print("Ingrese su nombre: ");
         String nombreUsuario = scanner.nextLine();
 
         Persona persona = new Persona(nombreUsuario);
 
-        System.out.println("Elige tu vehiculo (1 para Auto, 2 para Motocicleta, 3 para Bicicleta):");
+        System.out.println("Elige tu vehículo (1 para Auto, 2 para Motocicleta, 3 para Bicicleta):");
         int opcionVehiculo = scanner.nextInt();
         Vehiculo vehiculoElegido;
 
@@ -26,20 +26,21 @@ public class InteraccionUsuario {
                 vehiculoElegido = new Vehiculo("Bicicleta", 2.0);
                 break;
             default:
-                System.out.println("Opcion de vehiculo no valida, se utilizara un Auto por defecto.");
+                System.out.println("Opción de vehículo no válida, se utilizará un Auto por defecto.");
                 vehiculoElegido = new Vehiculo("Auto", 5.0);
                 break;
         }
-    }
-    // solicita el nombre del destino y la distancia en kilometros
+
+        // solicitar el nombre del destino y la distancia en kilómetros
         scanner.nextLine();
         System.out.print("Ingrese el destino: ");
-    String destino = scanner.nextLine();
-        System.out.print("Ingrese la distancia en kilometros: ");
-    double distanciaKilometros = scanner.nextDouble();
+        String destino = scanner.nextLine();
+        System.out.print("Ingrese la distancia en kilómetros: ");
+        double distanciaKilometros = scanner.nextDouble();
 
-    // elegir vehiculo y calcula el tiempo del viaje
+        // elegir vehículo y calcular el tiempo del viaje
         persona.elegirVehiculo(vehiculoElegido);
-    String mensaje = persona.viajarAlDestino(destino, distanciaKilometros);
+        String mensaje = persona.viajarAlDestino(destino, distanciaKilometros);
         System.out.println(mensaje);
+    }
 }
