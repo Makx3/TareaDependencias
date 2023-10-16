766e66993799cc46057b9ef0f30faebac0ca052b
Makx3 <gatoconb42@gmail.com>
2023-10-16 02:28:00 -0300

    Primer commit, clase persona.

---
+++ src/main/java/Persona.java
@@ -0,0 +1,22 @@
+public class Persona {
+    private String nombre;
+    private Vehiculo vehiculo;
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void elegirVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    public String viajarAlDestino(String destino, double distanciaKilometros) {
+        if (vehiculo != null) {
+            double tiempoMinutos = vehiculo.calcularTiempo(distanciaKilometros);
+            return "Hola " + nombre + " el tiempo de viaje aproximado para llegar a " + destino + " en " + vehiculo.getNombre() + " es de: " + tiempoMinutos + " minutos";
+        } else {
+            return "Eliga un vehiculo antes de viajar.";
+        }
+    }
+}
+
