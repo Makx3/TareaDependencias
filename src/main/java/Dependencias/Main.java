fb9c5ab1080c51972e07a021ecd1c680452f8a70
Makx3 <142506889+Makx3@users.noreply.github.com>
2023-10-16 04:30:32 -0300

    Update Main.java

--- src/main/java/Dependencias/Main.java
+++ src/main/java/Dependencias/Main.java
@@ -1,7 +1,5 @@
 package Dependencias;
 
-import Dependencias.InteraccionUsuario;
-
 public class Main {
     public static void main(String[] args) {
         InteraccionUsuario.ejecutarInteraccion();
